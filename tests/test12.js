cc93a5751aa17f8bfbefcfee3308b3e693819257
Quinton Ashley <qashto@gmail.com>
2022-10-16 09:05:31 -0500

    1.1.186

--- tests/test12.js
+++ tests/test12.js
@@ -1,24 +1,19 @@
-let player, gems;
+let player, pillar;
 
 function setup() {
-	createCanvas(160, 456);
+	createCanvas(60, 362);
+	world.gravity.y = 10;
 
-	gems = new Group();
-	gems.diameter = 10;
-	gems.x = () => random(0, width);
-	gems.y = () => random(0, height);
-	gems.amount = 80;
-
-	player = new Sprite();
-
-	player.overlap(gems, collect);
-}
-
-function collect(player, gem) {
-	gem.remove();
+	pillar = new Sprite(30, 362, 30, 500, 'static');
+	player = new Sprite(30, 0, 50);
+	player.bounciness = 0.4;
 }
 
 function draw() {
 	clear();
-	player.moveTowards(mouse.x, mouse.y);
+
+	if (mouse.presses()) player.vel.y = -5;
+	else if (player.collided(pillar)) {
+		pillar.h -= 52;
+	}
 }
