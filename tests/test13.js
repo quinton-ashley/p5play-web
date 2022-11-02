a969bd7849f2a9842d7cd92454e3781749d436b6
Quinton Ashley <qashto@gmail.com>
2022-11-02 16:37:18 -0500

    1.1.204

--- tests/test13.js
+++ tests/test13.js
@@ -1,25 +1,15 @@
-let ground, ball;
-
 function setup() {
 	createCanvas(500, 200);
-	world.gravity.y = 10;
 
-	//                 (x, y, [distance0, distance1, ...])
-	ground = new Sprite(
-		30,
-		90,
-		[
-			[20, 20],
-			[60, -10],
-			[50, 10]
-		],
-		'static'
-	);
-
-	ball = new Sprite(40, 0, 20);
+	//               (x, y, [length, angle, repeat])
+	let s = new Sprite([
+		[10, 10],
+		[100, 180],
+		[490, 100],
+		[10, 10]
+	]);
 }
 
 function draw() {
 	clear();
-	rect(ground.x - 2, ground.y - 2, 4, 4);
 }
