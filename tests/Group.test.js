fe2708773b89cf492d3cf0d7a8165de29cec1887
Caleb Foss <calebfoss@gmail.com>
2023-02-24 14:36:37 -0600

    Reformat spacing

--- tests/Group.test.js
+++ tests/Group.test.js
@@ -8,37 +8,42 @@ global.planck = require('planck');
 require('../v3/p5play.js');
 
 test('Group', () => {
-	const sketch = (p) => {
-		let g;
+  const sketch = (p) => {
+    let g;
 
-		p.setup = () => {
-			new p.Canvas(400, 400);
-			p.noLoop();
+    p.setup = () => {
+      new p.Canvas(400, 400);
+      p.noLoop();
 
-			expect(p).toHaveProperty('Group');
-			expect(p.Group).toBeInstanceOf(Function);
+      expect(p).toHaveProperty('Group');
+      expect(p.Group).toBeInstanceOf(Function);
 
-			g = new p.Group();
-			expect(g).toBeInstanceOf(p.Group);
+      g = new p.Group();
+      expect(g).toBeInstanceOf(p.Group);
 
-			new g.Sprite();
+      new g.Sprite();
 
-			g.customProp = 'foo';
-			expect(g.customProp).toBe('foo');
+      g.customProp = 'foo';
+      expect(g.customProp).toBe('foo');
 
-			new g.Sprite();
+      new g.Sprite();
 
-			expect(g[0].customProp).toBe(undefined);
-			expect(g[1].customProp).toBe('foo');
+      expect(g[0].customProp).toBe(undefined);
+      expect(g[1].customProp).toBe('foo');
 
-			g.life = 100;
+      g.life = 100;
 
-			expect(g[0].life).toBe(100);
+      expect(g[0].life).toBe(100);
 
-			g.removeAll();
+      const spriteConstructedOutsideGroup = new p.Sprite();
+      const spriteAdded = g.push(spriteConstructedOutsideGroup);
+      expect(spriteAdded).toBe(true);
+      expect(g.includes(spriteConstructedOutsideGroup)).toBe(true);
 
-			expect(g.length).toBe(0);
-		};
-	};
-	new p5(sketch);
+      g.removeAll();
+
+      expect(g.length).toBe(0);
+    };
+  };
+  new p5(sketch);
 });
