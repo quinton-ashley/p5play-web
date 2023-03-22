0d12a9aad2315087883f809b43f91bc97e9a4dc0
Quinton Ashley <qashto@gmail.com>
2023-03-22 12:32:41 -0500

    1.1.459

--- pro/pro.js
+++ pro/pro.js
@@ -1,13 +1,22 @@
 // Parse the URL to extract the id_token parameter
-let params = location.search;
-if (!params) params = '?' + location.hash.slice(1);
-const urlParams = new URLSearchParams(params);
-const idToken = urlParams.get('id_token');
+(async () => {
+	const log = console.log;
 
-if (idToken) {
+	// get the AWS Cognito id_token from the URL
+	let params = location.search;
+	if (!params) params = '?' + location.hash.slice(1);
+	const urlParams = new URLSearchParams(params);
+	const idToken = urlParams.get('id_token');
+
+	window.idToken = idToken; // for debugging
+
+	if (!idToken) return;
+
+	// hide the token from the URL
 	window.history.pushState(null, '', location.href.split(/[?#]/)[0]);
+
 	let user = jwt_decode(idToken);
-	console.log(user);
+	log(user);
 
 	// show section of page that requires authentication
 	document.getElementById('noauth').style.display = 'none';
@@ -15,4 +24,17 @@ if (idToken) {
 
 	// Set the user's name in the page
 	document.getElementById('username').innerHTML = user.email;
-}
+
+	let apiUrl = 'https://ntaknarhb9.execute-api.us-west-2.amazonaws.com/prod';
+
+	let userData = await (
+		await fetch(apiUrl + '/getUserData', {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${idToken}`
+			}
+		})
+	).json();
+
+	log(userData);
+})();
