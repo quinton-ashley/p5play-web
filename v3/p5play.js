a773a9af58f257930724ce3fbdc83b407e22539c
Quinton Ashley <qashto@gmail.com>
2023-11-06 13:54:21 -0500

    1.7.11

--- v3/p5play.js
+++ v3/p5play.js
@@ -3413,6 +3413,7 @@ p5.prototype.registerMethod('init', function p5playInit() {
 				if (target._isGroup) {
 					for (let s of target) {
 						s._hasOverlap[this._uid] = false;
+						this._hasOverlap[s._uid] = false;
 					}
 				}
 			}
@@ -3507,6 +3508,7 @@ p5.prototype.registerMethod('init', function p5playInit() {
 				if (target._isGroup) {
 					for (let s of target) {
 						s._hasOverlap[this._uid] = true;
+						this._hasOverlap[s._uid] = true;
 					}
 				}
 			}
@@ -5165,6 +5167,7 @@ p5.prototype.registerMethod('init', function p5playInit() {
 				this._hasOverlap[target._uid] = false;
 				for (let s of this) {
 					s._hasOverlap[target._uid] = false;
+					target._hasOverlap[s._uid] = false;
 				}
 			}
 			if (target._hasOverlap[this._uid] !== false) {
@@ -5172,8 +5175,10 @@ p5.prototype.registerMethod('init', function p5playInit() {
 				if (target._isGroup) {
 					for (let s of target) {
 						s._hasOverlap[this._uid] = false;
+						this._hasOverlap[s._uid] = false;
 						for (let s2 of this) {
 							s._hasOverlap[s2._uid] = false;
+							s2._hasOverlap[s._uid] = false;
 						}
 					}
 				}
@@ -5268,6 +5273,7 @@ p5.prototype.registerMethod('init', function p5playInit() {
 				this._hasOverlap[target._uid] = true;
 				for (let s of this) {
 					s._hasOverlap[target._uid] = true;
+					target._hasOverlap[s._uid] = true;
 				}
 			}
 			if (target._hasOverlap[this._uid] != true) {
@@ -5276,8 +5282,10 @@ p5.prototype.registerMethod('init', function p5playInit() {
 				if (target._isGroup) {
 					for (let s of target) {
 						s._hasOverlap[this._uid] = true;
+						this._hasOverlap[s._uid] = true;
 						for (let s2 of this) {
 							s._hasOverlap[s2._uid] = true;
+							s2._hasOverlap[s._uid] = true;
 						}
 					}
 				}
